dcc373730f49b76376e5a5942c37fceed68d5f3b
Diego Fernando Ortiz Gamboa <diegofergamboa@hotmail.es>
2022-05-25 20:23:19 -0500

    Creating the layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import React from "react";
+import { TodoCounter } from "./TodoCounter";
 // import './App.css';
+
+const defaultToDos = [
+  {text : 'Primera tarea' , completed : true} ,
+  {text : 'Segunda tarea' , completed : false} ,
+  {text : 'Tercera tarea' , completed : true} ,
+  {text : 'Cuarta tarea' , completed : false} ,
+];
+
 
 function App() {
   return (
+    <React.Fragment>
 
+      <TodoCounter />
+      {/* <TodoSearch />
+        <input placeholder='Search your tas to do'/>
+      <TodoList>
+        {defaultToDos.map(todo => (
+          <TodoItem/>
+          ))}
+      </TodoList> ,
+      <TodoCreateButton/>
+      <button>+</button> */}
+
+    </React.Fragment>
   );
 }
 
